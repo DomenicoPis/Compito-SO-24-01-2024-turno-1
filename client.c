5a5af7e41446ea700f268c1f54cc37be0a67a48e
DomenicoPis <domeni.piscopo@studenti.unina.it>
2026-02-12 12:31:23 +0100

    Update client.c

--- client.c
+++ client.c
@@ -29,8 +29,8 @@ int main() {
 
     // --- 2. PREPARAZIONE E INVIO RICHIESTA ---
     req.mtype = 1;              // Tipo messaggio (arbitrario, basta che sia > 0)
-    req.pid_client = getpid();  // Il mio PID (fondamentale per la risposta)
-    req.num_valori = num_valori;
+    req.pid_richiesta = getpid();  // Il mio PID (fondamentale per la risposta)
+    req.numero_valori = num_valori;
 
     printf("[CLIENT %d] Invio richiesta (num. valori: %d)\n", getpid(), num_valori);
 
